b0803b2273a08158f8d1281facb7a57c48f72dad
Sandeep <23mca20358@cuchd.in>
2025-05-14 22:45:45 +0530

    few change

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="/Sandeep_Yadav_Resume.pdf" download className="btn">
-        Download CV
-      </a>
+      <a href="/assets/SandyR.pdf" download className="btn">Download CV</a>
+
       <a href="#contact" className="btn btn-primary">
         Let's talk
       </a>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,16 +6,25 @@ import './header.css';
 const Header = () => {
   return (
     <header id="home">
+      {/* Fixed Top Navigation */}
+      <nav className="navbar">
+  <div className="navbar__logo">Sandeep </div>
+</nav>
+
+
+      {/* Header Content */}
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>SANDEEP YADAV</h1>
         <h5 className="text-yellow-500">(Front-end Developer)</h5>
 
-        <h4 className="text-light">"I am an aspiring Full Stack Developer, specializing in the MERN stack
-        to build scalable, efficient, and user-friendly applications. I have a
-        passion for solving real-world problems through clean, maintainable
-        code and modern web technologies."</h4>
-        
+        <h4 className="text-light">
+          "I am an aspiring Full Stack Developer, specializing in the MERN stack
+          to build scalable, efficient, and user-friendly applications. I have a
+          passion for solving real-world problems through clean, maintainable
+          code and modern web technologies."
+        </h4>
+
         <CTA />
         <a href="#contact" className="scroll__down">Scroll Down</a>
         <HeaderSocials />
